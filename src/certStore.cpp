8219732f167fb66a430d2083c1efda0a240ede1b
Jacek Banaszczyk <jacek.banaszczyk@gmail.com>
2021-02-27 04:54:33 +0100

    certStore: minor refactoring according to ReSharper, reformat introduced build setting `DEBUG_IOT_CERT_STORE`

--- src/certStore.cpp
+++ src/certStore.cpp
@@ -1,59 +1,58 @@
 #include "certStore.h"
 #include "generated/certificates.h"
-#include <memory>
+
+#if defined(DEBUG_IOT_CERT_STORE) && defined(DEBUG_IOT_PORT)
+#define LOG_CERT(...) DEBUG_IOT_PORT.printf_P( "[CERT] " __VA_ARGS__ )
+#else
+#define LOG_CERT(...)
+#endif
 
 namespace BearSSL {
+	void CertStore::installCertStore(br_x509_minimal_context *context) {
+		br_x509_minimal_set_dynamic(context, (void *) this, findHashedTA, freeHashedTA);
+	}
 
-void CertStore::installCertStore(br_x509_minimal_context *ctx) {
-  br_x509_minimal_set_dynamic(ctx, (void*)this, findHashedTA, freeHashedTA);
-}
+	const br_x509_trust_anchor *CertStore::findHashedTA(void *context, void *hashed_dn, size_t length) {
 
-const br_x509_trust_anchor *CertStore::findHashedTA(void *ctx, void *hashed_dn, size_t len) {
-    //compare sha256 from index file with hashed_dn
-    //then return certificate
+		//compare sha256 from index file with hashed_dn
+		//then return certificate
+		auto *pCertStore = static_cast<CertStore *>(context);
 
-    CertStore *cs = static_cast<CertStore *>(ctx);
+		if (!pCertStore || length != 32) {
+			return nullptr;
+		}
 
-    if (!cs || len != 32)
-    {
-        return nullptr;
-    }
+		for (uint16_t certIndex = 0; certIndex < numberOfCertificates; ++certIndex) {
+			if (!memcmp_P(hashed_dn, indices[certIndex], 32)) {
+				LOG_CERT("Certificate found at index %d\n", certIndex);
 
-    for (int i = 0; i < numberOfCertificates; i++)
-    {
-        if (!memcmp_P(hashed_dn, indices[i], 32))
-        { 
-            Serial.println(PSTR("Certificate found!"));
+				uint16_t certSize[1];
+				memcpy_P(certSize, certSizes + certIndex, 2);
 
-            uint16_t certSize[1];
-            memcpy_P(certSize, certSizes+i, 2);
+				auto *der = (uint8_t *) malloc(certSize[0]);
+				memcpy_P(der, certificates[certIndex], certSize[0]);
+				pCertStore->_x509 = new X509List(der, certSize[0]);
+				free(der);
 
-            uint8_t *der = (uint8_t *)malloc(certSize[0]);            
-            memcpy_P(der, certificates[i], certSize[0]);
-            cs->_x509 = new X509List(der, certSize[0]);
-            free(der);
-            
-            if (!cs->_x509)
-            {
-                 return nullptr;
-            }
+				if (!pCertStore->_x509) {
+					return nullptr;
+				}
 
-            br_x509_trust_anchor *ta = (br_x509_trust_anchor *)cs->_x509->getTrustAnchors();
-            memcpy_P(ta->dn.data, indices[i], 32);
-            ta->dn.len = 32;
+				auto *trust_anchor = (br_x509_trust_anchor *) pCertStore->_x509->getTrustAnchors();
+				memcpy_P(trust_anchor->dn.data, indices[certIndex], 32);
+				trust_anchor->dn.len = 32;
 
-            return ta;
-        }
-    }
+				return trust_anchor;
+			}
+		}
 
-    return nullptr;
-}
+		return nullptr;
+	}
 
-void CertStore::freeHashedTA(void *ctx, const br_x509_trust_anchor *ta) {
-  CertStore *cs = static_cast<CertStore*>(ctx);
-  (void) ta; // Unused
-  delete cs->_x509;
-  cs->_x509 = nullptr;
-}
-
+	void CertStore::freeHashedTA(void *ctx, const br_x509_trust_anchor *ta) {
+		auto *pCertStore = static_cast<CertStore *>(ctx);
+		(void) ta; // Unused
+		delete pCertStore->_x509;
+		pCertStore->_x509 = nullptr;
+	}
 }
--- src/certStore.h
+++ src/certStore.h
@@ -11,22 +11,21 @@
 
 namespace BearSSL {
 
-class CertStore {
-  public:
-    CertStore() { };   
+	class CertStore {
+	public:
+		CertStore() {};
 
-    // Installs the cert store into the X509 decoder (normally via static function callbacks)
-    void installCertStore(br_x509_minimal_context *ctx);
+		// Installs the cert store into the X509 decoder (normally via static function callbacks)
+		void installCertStore(br_x509_minimal_context *context);
 
-  protected:
-    X509List *_x509 = nullptr;
+	protected:
+		X509List *_x509 = nullptr;
 
-    // These need to be static as they are callbacks from BearSSL C code
-    static const br_x509_trust_anchor *findHashedTA(void *ctx, void *hashed_dn, size_t len);
-    static void freeHashedTA(void *ctx, const br_x509_trust_anchor *ta);
-};
+		// These need to be static as they are callbacks from BearSSL C code
+		static const br_x509_trust_anchor *findHashedTA(void *context, void *hashed_dn, size_t length);
 
+		static void freeHashedTA(void *ctx, const br_x509_trust_anchor *ta);
+	};
 };
 
 #endif
-
